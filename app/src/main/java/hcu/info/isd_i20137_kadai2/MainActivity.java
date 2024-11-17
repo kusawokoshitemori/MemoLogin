8da13af7328e863362785c8c3bb8f4f4e247603f
草をこして森 <kyou0317kyou0317@gmail.com>
2024-11-17 19:00:59 +0900

    簡単な画面遷移追加

--- app/src/main/java/hcu/info/isd_i20137_kadai2/MainActivity.java
+++ app/src/main/java/hcu/info/isd_i20137_kadai2/MainActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // ボタン取得
+        Button buttonPlayerSubmitTransition = findViewById(R.id.buttonPlayerSubmitTransition);
+
+        // ボタンを押すと SubmitActivity に遷移する機能
+        buttonPlayerSubmitTransition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubmitActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/hcu/info/isd_i20137_kadai2/SubmitActivity.java
+++ app/src/main/java/hcu/info/isd_i20137_kadai2/SubmitActivity.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class SubmitActivity extends AppCompatActivity {
 
@@ -19,6 +23,20 @@ public class SubmitActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // ボタン取得
+        Button buttonHomeBack = findViewById(R.id.buttonHomeBack);
+
+        // ボタンを押すと MainActivity に遷移する機能
+        buttonHomeBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
         });
     }
 }
